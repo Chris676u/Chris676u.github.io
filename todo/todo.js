f5393cdceccf3353cabbab76e9876bfd1dd03ca7
Chris676u <36342737+Chris676u@users.noreply.github.com>
2022-11-10 12:09:31 -0500

    Everything

---
+++ todo/todo.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const $ = document.querySelector.bind(document);
+
+
+function createNewTodo(){
+   var d = document.createElement('div'); // create div, d
+   $('.items').appendChild( d )
+   d.innerHTML = $('.newtodo').value;
+   $('.newtodo').value = '';
+   // add onclick functionality to d
+   d.onclick = function(){
+      d.className = 'done';
+    };
+}
